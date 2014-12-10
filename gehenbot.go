c1dd75facffdef3190daaa610fd95191b182e40b
Mike 'Fuzzy' Partin <fuzzy@fu-manchu.org>
2014-12-10 04:18:54 -0800

    added Log Debug and Fatal convenience functions that respect cfg.Verbose and cfg.Debug

--- gehenbot.go
+++ gehenbot.go
@@ -1,39 +1,69 @@
+/*
+Copyright (c) 2014, Mike 'Fuzzy' Partin <fuzzy@fumanchu.org>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 package main
 
 import (
-	"fmt"
+	// stdlib
 	"log"
 	"time"
 )
 
-// globals that make me ill, move to configs asap
 var (
-	bots         []BotInstance
 	cfg          Config
 	GehenVersion = "0.2.0"
-	daemon       = false
-	verbose      = false
+	GehenAuthor  = "Mike 'Fuzzy' Partin <fuzzy@fumanchu.org>"
 )
+
+func Log(line string) {
+	if cfg.Verbose {
+		log.Println(line)
+	}
+}
+
+func Debug(line string) {
+	if cfg.Debug {
+		log.Println(line)
+	}
+}
+
+func Fatal(line string) { log.Fatalln(line) }
 
 // main program entry
 
 func main() {
-
-	// if we have been directed to daemonize, then we need to do so
-	if daemon {
-		log.Println("We should be daemonizing")
-	}
-
-	// if we are supposed to be verbose, we need to pass that off to the BotInstance objects
-	if verbose {
-		log.Println("We should be logging")
-	}
+	var bots []BotInstance // this shouldn't really need a note
 
 	// get our config data
-	ReadConfig("./gehenbot.json")
+	cfg = ReadConfig("./gehenbot.json")
 
-	// print our little banner
-	fmt.Printf("\nGehenBot v%s by Mike 'Fuzzy' Partin <fuzzy@fumanchu.org>\n\n", GehenVersion)
+	// if we have been directed to daemonize, then we need to do so
+	if cfg.Daemonize {
+		Log("We should be daemonizing")
+	}
 
 	// set our config data
 	for _, b := range cfg.Bots {
