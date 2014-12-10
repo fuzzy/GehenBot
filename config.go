97b6b128d21740f29445b43842972cd2b5fa426d
Mike 'Fuzzy' Partin <fuzzy@fu-manchu.org>
2014-12-10 04:14:34 -0800

    Moved daemonize verbose and debug flags to the config file, got rid of direct log.* calls

--- config.go
+++ config.go
@@ -1,12 +1,42 @@
+/*
+Copyright (c) 2014, Mike 'Fuzzy' Partin <fuzzy@fumanchu.org>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 package main
 
 import (
+	// stdlib
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
-	"log"
 )
 
 type Config struct {
+	Daemonize   bool   `json:"daemonize"`
+	Verbose     bool   `json:"verbose"`
+	Debug       bool   `json:"debug"`
 	CommandChar string `json:"command_char"`
 	CommandDir  string `json:"command_dir"`
 	PluginDir   string `json:"plugin_dir"`
@@ -19,14 +49,18 @@ type Config struct {
 	} `json:"bots"`
 }
 
-func ReadConfig(jsonCfg string) {
+func ReadConfig(jsonCfg string) Config {
+	var cfg Config
+
 	cfgData, err := ioutil.ReadFile(jsonCfg)
 	if err != nil {
-		log.Fatalf("Error in io.ReadFile(): %s\n", err)
+		Fatal(fmt.Sprintf("Error in io.ReadFile(): %s\n", err))
 	}
 
 	err = json.Unmarshal([]byte(cfgData), &cfg)
 	if err != nil {
-		log.Fatalf("Error in ReadConfig(): %s\n", err)
+		Fatal(fmt.Sprintf("Error in ReadConfig(): %s\n", err))
 	}
+
+	return cfg
 }
