ff0c8924614f2aa521d5337f3ada7cdc287bcf0c
Mike 'Fuzzy' Partin <fuzzy@fu-manchu.org>
2014-12-13 01:28:04 -0800

    got rid of the profanity in the greet.lua plugin

--- plugins/greet.lua
+++ plugins/greet.lua
@@ -2,5 +2,5 @@
 register_handler("JOIN", "greet")
 
 function greet(nick, event, target, args)
-	privmsg(target, string.format("Hello %s, you fucknut.", nick))
+	privmsg(target, string.format("Hello %s, you...you have a sister as well.", nick))
 end
